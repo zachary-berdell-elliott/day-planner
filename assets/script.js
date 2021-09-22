2508797f10415f2c69226f694fa17bd38f2097a7
zachary <zacharyberdell@gmail.com>
2021-09-22 17:41:47 -0600

    working on getting the blocks to show up.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
 moment().format();
+var hourForm = $("form").addClass("time-block");
+var inputArray = [];
+
 //outputs the current day at the top of the page
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+//Displays the elements on the page
+for (let i=1; i<25; i++) {
+    $("#block-storage").append(hourForm);
+}
