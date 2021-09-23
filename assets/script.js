6b27fab8ed681ff721314587ca93759643c8218e
zachary <zacharyberdell@gmail.com>
2021-09-23 14:01:35 -0600

    WOrking on getting the if else statements and id assignment to work properly.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-moment().format();
 var hourForm = $("<div>").addClass("time-block");
 var hourLable = $("<p>").addClass("hour");
 var textEntry = $("<textarea>");
@@ -10,12 +9,28 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 //Displays the elements on the page by creating a timeblock for each hour in the day and appending the required elements to it.
 for (let i=0; i<25; i++) {
-    $("#block-storage").append(hourForm.clone().attr("id", "block"+i.toString()));
-    hourForm.append(hourLable.text(moment(i.toString(), "k").format("hA")));
-    hourForm.append(textEntry);
-    hourForm.append(saveBtn);
+    let stringI = i.toString();
+    let idAssign = "block"+stringI;
+    let iterateHour = moment(stringI, "k");
+    let currentHour = moment().format("k");
+
+    $("#block-storage").append(hourForm.clone().attr("id", idAssign));
+    idAssign = $("#" + idAssign);
+    idAssign.append(hourLable.text(iterateHour.format("hA")));
+    idAssign.append(textEntry);
+    idAssign.append(saveBtn);
 
     //decides the color and sets the appropriate tasks based on whether the hour-block is current, present or future
+    if (iterateHour < currentHour) {
+        idAssign.addClass("past");
+    }
+    else if (iterateHour > currentHour) {
+        idAssign.addClass("future");
+    }
+    else {
+        idAssign.addClass("present");
+    }
+
 
     //saves the user input to the input array
 }
