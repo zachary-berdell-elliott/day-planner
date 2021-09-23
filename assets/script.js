a232d7f0b52b6e0759417889823f82c642bdc523
zachary <zacharyberdell@gmail.com>
2021-09-23 16:37:09 -0600

    Got the classes to be assigned but not properly

--- assets/script.js
+++ assets/script.js
@@ -9,17 +9,22 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 //Displays the elements on the page by creating a timeblock for each hour in the day and appending the required elements to it.
 for (let i=0; i<25; i++) {
+    //variable assignment
     let stringI = i.toString();
     let idAssign = "block"+stringI;
     let iterateHour = moment(stringI, "k");
     let currentHour = moment().format("k");
 
+    //Clones and appends the neccessary elements to the time block storage
     $("#block-storage").append(hourForm.clone().attr("id", idAssign));
-    idAssign = $("#" + idAssign);
-    idAssign.append(hourLable.text(iterateHour.format("hA")));
-    idAssign.append(textEntry);
-    idAssign.append(saveBtn);
+    hourForm.append(hourLable.text(iterateHour.format("hA")));
+    hourForm.append(textEntry);
+    hourForm.append(saveBtn);
 
+    //variable reassignment 
+    idAssign = $("#" + idAssign);
+    iterateHour = iterateHour.format("k");
+    
     //decides the color and sets the appropriate tasks based on whether the hour-block is current, present or future
     if (iterateHour < currentHour) {
         idAssign.addClass("past");
@@ -29,8 +34,9 @@ for (let i=0; i<25; i++) {
     }
     else {
         idAssign.addClass("present");
-    }
-
+    } 
+    console.log(iterateHour);
+    console.log(currentHour);
 
     //saves the user input to the input array
 }
