d064c77f01c172df99a9ec856f030763220ae61d
Zachary Elliott <zacharyberdell@gmail.com>
2021-09-25 14:39:57 -0600

    Syntax and variable declaration areas fixed. Hours reduced to standard work week.

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,55 @@
-var hourForm = $("<div>").addClass("time-block row");
-var hourLable = $("<p>").addClass("hour");
-var textEntry = $("<textarea>").addClass("description");
-var saveBtn = $("<button>").addClass("saveBtn").text("Save");
+
+
 var inputArray = [];
 
 //outputs the current day at the top of the page
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 //Displays the elements on the page by creating a timeblock for each hour in the day and appending the required elements to it.
-for (let i=0; i<=24; i++) {
+for (let i=0; i<=8; i++) {
     //variable assignment
-    let stringI = i.toString();
+    var hourForm = $("<div>").addClass("time-block row");
+    var hourLable = $("<p>").addClass("hour");
+    var textEntry = $("<textarea>").addClass("description");
+    var saveBtn = $("<button>").addClass("saveBtn").text("Save");
+    let timeDisplayed = i + 9;
+    let stringI = timeDisplayed.toString();
     let idAssign = "block"+stringI;
     let iterateHour = moment(stringI, "k");
     let currentHour = moment().format("k");
 
+
     //Clones and appends the neccessary elements to the time block storage
-    $("#block-storage").append(hourForm.clone().attr("id", idAssign));
+
+
     hourForm.attr("number", i);
     hourForm.append(hourLable.text(iterateHour.format("hA")));
     hourForm.append(textEntry);
     hourForm.append(saveBtn);
+    hourForm.attr("id", idAssign);
+    
 
     //variable reassignment 
-    idAssign = $("#" + idAssign);
+    //idAssign = $("#" + idAssign);
     iterateHour = iterateHour.format("k");
     
     //decides the color and sets the appropriate tasks based on whether the hour-block is current, present or future
     if (currentHour > iterateHour) {
-        idAssign.addClass("past");
+        hourForm.addClass("past");
     }
     else if (currentHour < iterateHour) {
-        idAssign.addClass("future");
+        hourForm.addClass("future");
     }
     else {
-        idAssign.addClass("present");
+        hourForm.addClass("present");
     } 
+    $("#block-storage").append(hourForm);
     console.log(iterateHour);
     console.log(currentHour);
 
     //sets the users saved input up
 }
-$("#block0").remove();
+//$("#block0").remove();
 
 saveBtn.click(function(){
     parentNumber = saveBtn.parent().attr("number");
